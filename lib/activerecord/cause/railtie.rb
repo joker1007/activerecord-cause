d6bf890a530dfa2762335457f9b0a6166f0a68f5
joker1007 <kakyoin.hierophant@gmail.com>
2015-04-16 15:06:13 +0900

    Fix default match_paths for Rails

--- lib/activerecord/cause/railtie.rb
+++ lib/activerecord/cause/railtie.rb
@@ -2,7 +2,7 @@ module ActiveRecord
   module Cause
     class Railtie < ::Rails::Railtie
       initializer "activerecord-cause" do
-        ActiveRecord::Cause.match_paths = [/#{Rails.root.join("app").to_s}/]
+        ActiveRecord::Cause.match_paths = [/#{Regexp.quote(Rails.root.join("app").to_s)}/]
       end
     end
   end
