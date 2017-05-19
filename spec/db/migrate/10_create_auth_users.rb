d66833b84211c1aa40fd7419b6e859012fb88fa3
joker1007 <kakyoin.hierophant@gmail.com>
2017-05-19 19:51:44 +0900

    Fix migration for test

--- spec/db/migrate/10_create_auth_users.rb
+++ spec/db/migrate/10_create_auth_users.rb
@@ -1,9 +1,21 @@
-class CreateAuthUsers < ActiveRecord::Migration
-  def change
-    create_table :auth_users do |t|
-      t.string :name
+if ActiveRecord.version < Gem::Version.new("5.0.0")
+  class CreateAuthUsers < ActiveRecord::Migration
+    def change
+      create_table :auth_users do |t|
+        t.string :name
 
-      t.timestamps null: false
+        t.timestamps null: false
+      end
+    end
+  end
+else
+  class CreateAuthUsers < ActiveRecord::Migration[5.0]
+    def change
+      create_table :auth_users do |t|
+        t.string :name
+
+        t.timestamps null: false
+      end
     end
   end
 end
--- spec/db/migrate/20_create_users.rb
+++ spec/db/migrate/20_create_users.rb
@@ -1,10 +1,23 @@
-class CreateUsers < ActiveRecord::Migration
-  def change
-    create_table :users do |t|
-      t.string :name
-      t.references :auth_user, polymorphic: true
+if ActiveRecord.version < Gem::Version.new("5.0.0")
+  class CreateUsers < ActiveRecord::Migration
+    def change
+      create_table :users do |t|
+        t.string :name
+        t.references :auth_user, polymorphic: true
 
-      t.timestamps null: false
+        t.timestamps null: false
+      end
+    end
+  end
+else
+  class CreateUsers < ActiveRecord::Migration[5.0]
+    def change
+      create_table :users do |t|
+        t.string :name
+        t.references :auth_user, polymorphic: true
+
+        t.timestamps null: false
+      end
     end
   end
 end
